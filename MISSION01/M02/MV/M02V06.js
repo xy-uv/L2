135ec89cc1b3b82463e6cb715822bdb12fdde2b4
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-04 10:55:06 +0600

    Add initial implementation for M02V06.js with lookup table generation

---
+++ MISSION01/M02/MV/M02V06.js
@@ -0,0 +1,17 @@
+//* Generate a lookup table
+
+//? Input
+const postsArray = [
+  { id: "p-101", title: "Intro to SQL", author: "Alex" },
+  { id: "p-102", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-103", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-104", title: "CSS Grid Tricks", author: "Alex" },
+];
+
+//? Output
+// {
+//   "p-101": { "id": "p-101", "title": "Intro to SQL", "author": "Alex" },
+//   "p-102": { "id": "p-102", "title": "Data Structures in JS", "author": "Beth" },
+//   "p-103": { "id": "p-103", "title": "Understanding Reduce", "author": "Chris" },
+//   "p-104": { "id": "p-104", "title": "CSS Grid Tricks", "author": "Alex" }
+// }
