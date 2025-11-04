77b457b172ce7732f5ff29a279364fbffda9f8f3
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-04 11:41:29 +0600

    Add initial implementation for M02P07.js and M02V07.js with survey response aggregation

---
+++ MISSION01/M02/MP/M02P07.js
@@ -0,0 +1,24 @@
+//* Grouping and Aggregating Data
+
+// Scenario: Count every survey and group by response
+
+//? input
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+//? Output
+// { A: 5, C: 3, B: 5, D: 1 }
---
+++ MISSION01/M02/MV/M02V07.js
@@ -0,0 +1,35 @@
+//* Grouping and Aggregating Data
+
+// Scenario: Count every survey and group by response
+
+//? input
+const surveyResponses = [
+  "A",
+  "C",
+  "B",
+  "A",
+  "B",
+  "B",
+  "C",
+  "A",
+  "B",
+  "D",
+  "A",
+  "C",
+  "B",
+  "A",
+];
+
+//? Output
+// { A: 5, C: 3, B: 5, D: 1 }
+const survey = surveyResponses.reduce((acc, ans) => {
+  //   if (acc[ans]) {
+  //     acc[ans] += 1;
+  //   } else {
+  //     acc[ans] = 1;
+  //   }
+  acc[ans] = (acc[ans] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(survey);
