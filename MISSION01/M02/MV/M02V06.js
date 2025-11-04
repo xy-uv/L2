833232e38e5aa06577aca90e498920f4853bee3f
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-04 11:27:24 +0600

    Refactor M02V06.js to implement lookup table generation using map and reduce methods

--- MISSION01/M02/MV/M02V06.js
+++ MISSION01/M02/MV/M02V06.js
@@ -15,3 +15,16 @@ const postsArray = [
 //   "p-103": { "id": "p-103", "title": "Understanding Reduce", "author": "Chris" },
 //   "p-104": { "id": "p-104", "title": "CSS Grid Tricks", "author": "Alex" }
 // }
+
+//! Using map
+const output = {};
+postsArray.map((post) => (output[post.id] = post));
+console.log(output);
+
+const lookup = postsArray.reduce((acc, post) => {
+  acc[post.id] = post;
+  return acc;
+}, {});
+
+// console.log(lookup);
+for (let id in lookup) [console.log(lookup[id])];
