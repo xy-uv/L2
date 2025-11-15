e14a6fbf146d78cfdc50c7d57de6f592712baf91
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-15 07:40:17 +0600

    Add TypeScript example demonstrating keyof constraints and object property access

---
+++ MISSION02/M06/MV/M06V04.ts
@@ -0,0 +1,45 @@
+//* Keyof constrains
+
+type RichVehicle = {
+  car: string;
+  bike: string;
+  bus: string;
+};
+
+const richPeopleVehicle: RichVehicle = {
+  car: "Audi",
+  bike: "Yemaha",
+  bus: "Mercediz",
+};
+
+type MyVehicle1 = "car" | "bike" | "bus";
+type MyVehicle2 = keyof RichVehicle;
+
+const myVehicle1: MyVehicle1 = "bike";
+const myVehicle2: MyVehicle2 = "car";
+
+type TUser = {
+  id: number;
+  name: string;
+  address: {
+    city: string;
+  };
+};
+const user: TUser = {
+  id: 12369,
+  name: "Shifat",
+  address: {
+    city: "Rangpur",
+  },
+};
+
+//? const myId = user.id;
+//? const myId = user["id"];
+//? const myName = user["name"];
+//? const address = user["address"];
+
+//? console.log({ myId, myName, address });
+
+const getPropertyFromObj = <X>(obj: X, key: keyof X) => {
+  return obj[key];
+};
