3a7343e719e69f03be54124cafa294a2553a41eb
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-28 07:53:54 +0600

    Refactor request handling to use route mapping for improved scalability

--- MISSION03/M11/MV/src/server.ts
+++ MISSION03/M11/MV/src/server.ts
@@ -1,26 +1,45 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import variable from "./config";
+import { routes, TRouteHandler } from "./helpers/route_handler";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    if (req.method === "GET" && req.url === "/") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Hello, World!" }));
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
+    const map = routes.get(method);
+    const handler: TRouteHandler | undefined = map?.get(path);
+
+    if (handler) {
+      handler(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          success: false,
+          message: "Request url not found",
+          path_url: path,
+        })
+      );
     }
-    if (req.method === "GET" && req.url === "/status") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ status: "OK" }));
-    }
-    if (req.method === "POST" && req.url === "/echo") {
-      let body = "";
-      req.on("data", (chunk) => {
-        body += chunk;
-      });
-      req.on("end", () => {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ echo: body }));
-      });
-    }
+
+    //   if (req.method === "GET" && req.url === "/") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     res.end(JSON.stringify({ message: "Hello, World!" }));
+    //   }
+    //   if (req.method === "GET" && req.url === "/status") {
+    //     res.writeHead(200, { "Content-Type": "application/json" });
+    //     res.end(JSON.stringify({ status: "OK" }));
+    //   }
+    //   if (req.method === "POST" && req.url === "/echo") {
+    //     let body = "";
+    //     req.on("data", (chunk) => {
+    //       body += chunk;
+    //     });
+    //     req.on("end", () => {
+    //       res.writeHead(200, { "Content-Type": "application/json" });
+    //       res.end(JSON.stringify({ echo: body }));
+    //     });
+    //   }
   }
 );
 
