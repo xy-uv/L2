d786f1a5592c7b14e6ddeae39eabad8a8dc376b5
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2026-01-06 21:42:32 +0600

    feat(pagination): add pagination sorting helpers for managing page, limit, sorting options

---
+++ Projects/Blog/src/helpers/pagination.sorting.helpers.ts
@@ -0,0 +1,31 @@
+export type TOptions = {
+  page?: string | number;
+  limit?: string | number;
+  sortBy?: string;
+  sortOrder?: string;
+};
+
+export interface IOptionsResult {
+  page: number;
+  limit: number;
+  skip: number;
+  sortBy: string;
+  sortOrder: string;
+}
+
+export const paginationSortingHelpers = (options: TOptions): IOptionsResult => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
+
+  return {
+    page,
+    limit,
+    skip,
+    sortBy,
+    sortOrder,
+  };
+};
